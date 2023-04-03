46b648540f479466582ec93a1482ec8acf651756
lugfigue <lugfigue@bancolombia.com.co>
2023-04-03 14:50:27 -0500

    entidad Hero

---
+++ domain/model/src/main/java/co/com/lgfh/model/hero/Hero.java
@@ -0,0 +1,16 @@
+package co.com.lgfh.model.hero;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Hero {
+    private int id;
+    private String name;
+}
---
+++ domain/model/src/main/java/co/com/lgfh/model/hero/gateways/HeroRepository.java
@@ -0,0 +1,4 @@
+package co.com.lgfh.model.hero.gateways;
+
+public interface HeroRepository {
+}
